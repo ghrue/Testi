142465c97fa4241a5ce4d564f540f3703e07e33f
Kalle <kalle.kananen@gmail.com>
2018-11-02 12:59:22 +0200

    jep

--- Harjoitus3/bootstrap.sh
+++ Harjoitus3/bootstrap.sh
@@ -1,6 +1,6 @@
 sudo apt-get -y install git
 sudo mkdir /var/tmp/demo
 cd /var/tmp/demo
-sudo git clone https://github.com/ghrue/Testi/tree/Dev
-sudo chmod u+x Testi/Harjoitus\ 3/Vagrant/test.sh
-sudo Testi/Harjoitus\ 3/Vagrant/test.sh
+sudo git clone https://github.com/ghrue/Testi.git
+sudo chmod u+x Testi/Harjoitus3/testi.sh
+sudo Testi/Harjoitus3/testi.sh
