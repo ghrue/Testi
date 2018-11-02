5e680604cfb2c6ddc66349847a2d717af3ab2985
Kalle <kalle.kananen@gmail.com>
2018-11-02 13:58:25 +0200

    asdf

--- Harjoitus3/docker-training.sh
+++ Harjoitus3/docker-training.sh
@@ -4,5 +4,5 @@ sudo apt-get -y install git
 sudo mkdir /var/tmp/dockertraining
 cd /var/tmp/dockertraining
 sudo git clone https://github.com/docker-training/webapp
-sudo docker build -t testwebapp .
+sudo docker build -t testwebapp webapp
 sudo docker run -d -p 80:5000 testwebapp
