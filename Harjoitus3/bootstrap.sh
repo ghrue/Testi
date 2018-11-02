bb2d632709b2a0d4c83ce5fbdb7759e8ca08d3ff
Kalle <kalle.kananen@gmail.com>
2018-11-02 12:45:04 +0200

    Vagrant files

---
+++ Harjoitus3/bootstrap.sh
@@ -0,0 +1,6 @@
+sudo apt-get -y install git
+sudo mkdir /var/tmp/demo
+cd /var/tmp/demo
+sudo git clone https://github.com/ghrue/Testi/tree/Dev
+sudo chmod u+x Testi/Harjoitus\ 3/Vagrant/test.sh
+sudo Testi/Harjoitus\ 3/Vagrant/test.sh
